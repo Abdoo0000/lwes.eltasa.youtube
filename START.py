42b414df34a52b67c8d42397609a79fa864169dc
Abdoo0000 <70906199+Abdoo0000@users.noreply.github.com>
2020-10-23 16:18:23 +0200

    Update START.py

--- START.py
+++ START.py
@@ -1,10 +1,10 @@
-print ("\033[0;31m ☞⇉⇉⇉⇉⇉⇉⇉⇉☜")
-print ("\033[0;32m ★              ★")
-print ("\033[0;33m ★ LWES-ELTASA  ★")
-print ("\033[0;34m ★              ★")
-print ("\033[0;31m ☞⇉⇉⇉⇉⇉⇉⇉⇉☜
-print ("\033[0;32m        ▒▒")
-print ("\033[0;33m ▓▓▓▓▓▓▓▓▓▓▓▓▓")
+import os 
+os.system ("clear")
+print ("\033[0;31m ################")
+print ("\033[0;32m L  W  E  S              ")
+print ("\033[0;33m E  L  T  A  S  A      ")
+print ("\033[0;34m E  L  K  B  E  R      ")
+print ("\033[0;33m ################")
 import webbrowser
 import time
 n = input("\033[0;31m inter-the-url: ")
