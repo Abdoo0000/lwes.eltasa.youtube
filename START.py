3eeb5859312f073f458db15efea395dfbcd0ae81
Abdoo0000 <70906199+Abdoo0000@users.noreply.github.com>
2020-10-23 16:01:22 +0200

    Create START.py

---
+++ START.py
@@ -0,0 +1,13 @@
+print ("\033[0;31m ☞⇉⇉⇉⇉⇉⇉⇉⇉☜")
+print ("\033[0;32m ★              ★")
+print ("\033[0;33m ★ LWES-ELTASA  ★")
+print ("\033[0;34m ★              ★")
+print ("\033[0;31m ☞⇉⇉⇉⇉⇉⇉⇉⇉☜
+print ("\033[0;32m        ▒▒")
+print ("\033[0;33m ▓▓▓▓▓▓▓▓▓▓▓▓▓")
+import webbrowser
+import time
+n = input("\033[0;31m inter-the-url: ")
+m = 0
+while m < 100 :
+	webbrowser.open(n)
